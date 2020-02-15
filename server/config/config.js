4823873a9d9f61879a62aff8d226e6203456d8e9
sportgift developer <sport.gift.io@gmail.com>
2020-02-15 16:01:32 -0500

    cambios en config file

--- server/config/config.js
+++ server/config/config.js
@@ -12,10 +12,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //  Base de datos
 // =======================
 let urlDB;
-//if (process.env.NODE_ENV === 'dev') {
-//  urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
-urlDB = 'mongodb+srv://root_mongodb:LJWUzgTAXzjNSYWj@cluster0-liv4w.mongodb.net/cafe'
-    //}
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://root_mongodb:LJWUzgTAXzjNSYWj@cluster0-liv4w.mongodb.net/cafe'
+}
 
 process.env.URLDB = urlDB;
