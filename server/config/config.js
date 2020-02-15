fc1d59855b1c3928c5efebab279242927d734ed4
sportgift developer <sport.gift.io@gmail.com>
2020-02-15 16:14:24 -0500

    variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://root_mongodb:LJWUzgTAXzjNSYWj@cluster0-liv4w.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
